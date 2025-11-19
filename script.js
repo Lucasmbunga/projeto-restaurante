a5632d499ff8c3483d7f5b537db07340211fdefe
Lucas Mbunga António Pascoal <bernivaldolucaspascoal@gmail.com>
2025-11-19 17:17:42 +0100

    Adicionei o formulário de cadastro

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const menuFixo = document.querySelector(".menu-fixo")
 const navLinks = document.querySelectorAll("nav a")
 
-const sections=document.querySelectorAll
-("section")
+const sections = document.querySelectorAll
+    ("section")
+const botaoCadastrar = document.querySelector(".botaoCadastrar")
+const overlay = document.querySelector(".overlay")
+
+
+botaoCadastrar.addEventListener("click", () => {
+    document.querySelector("dialog").show()
+    overlay.style.display = "block"
+})
 
 window.onscroll = function () {
     window.scrollY > 50 ? menuFixo.classList.add("fixar") : menuFixo.classList.remove("fixar")
+    controlarSecoes()
 }
 
 navLinks.forEach(link => {
@@ -19,4 +28,21 @@ navLinks.forEach(link => {
     })
 })
 
+const controlarSecoes = () => {
+    sections.forEach(section => {
+        const posicaoRolagem = window.scrollY
+        const topSection = section.offsetTop - 50
+        const alturaSection = section.offsetHeight
+        const id = section.getAttribute("id")
+        if (posicaoRolagem >= topSection && posicaoRolagem < topSection + alturaSection) {
 
+            navLinks.forEach(link => {
+                link.classList.remove("ativo")
+            })
+            const linkAtivo = document.querySelector("nav a[href*=" + id + "]")
+            if (linkAtivo) {
+                linkAtivo.classList.add("ativo")
+            }
+        }
+    })
+}    
